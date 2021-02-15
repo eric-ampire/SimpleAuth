03c7d20231ff37ee2b627cad0477aff0fe8d1d15
Eric Ampire <ericampire.top@gmail.com>
2021-02-16 01:34:36 +0200

    Update Lib

--- app/src/main/java/com/jaychang/demo/sa/ProfileActivity.kt
+++ app/src/main/java/com/jaychang/demo/sa/ProfileActivity.kt
@@ -3,8 +3,8 @@ package com.jaychang.demo.sa
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.jaychang.sa.SocialUser
 import com.jaychang.sa.facebook.SimpleAuth
 import kotlinx.android.synthetic.main.activity_profile.*
